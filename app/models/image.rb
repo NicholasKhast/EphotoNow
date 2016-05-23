9db3dc5798610c4255663709b336d420fcb7fbc5
pouyakhast <pkhast@yahoo.com>
2016-05-23 09:57:08 +0000

    prod modification

--- app/models/image.rb
+++ app/models/image.rb
@@ -4,7 +4,7 @@ class Image < ActiveRecord::Base
   
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" },   
     :storage => :s3,
-    :s3_credentials => "#{Rails.root}/config/s3.yml",
+    
     :path => ":attachment/:id/:style.:extension",
     :bucket => "ephotonowapp"
   
